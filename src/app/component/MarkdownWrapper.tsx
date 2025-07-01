dd9b3e5e4f143a3cd3693f2efda7495d07b95edd
RitickLath <ritick943lath@gmail.com>
2025-07-01 20:41:21 +0530

    Theme Managed For Md

--- src/app/component/MarkdownWrapper.tsx
+++ src/app/component/MarkdownWrapper.tsx
@@ -1,19 +1,16 @@
-// Need to manage the light and dark theme part and "prone" so we can adjust the headings size based on screen size.
+// Need to manage "prone" so we can adjust the headings size based on screen size.
 
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import MarkdownPreview from "@uiw/react-markdown-preview";
 
 const MarkdownWrapper = ({ children }: { children: string }) => {
-  const [theme, setTheme] = useState<"light" | "dark">("dark");
-
   return (
     <div style={{ backgroundColor: "transparent" }}>
       <MarkdownPreview
         className="list-disc list-inside [&>ul]:list-disc [&>ul]:-ml-3"
         source={children}
-        wrapperElement={{ "data-color-mode": theme }}
         style={{ backgroundColor: "transparent" }}
       />
     </div>
--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -9,11 +9,17 @@ const Navbar = () => {
 
   const toggleTheme = () => {
     document.documentElement.classList.toggle("light");
+    if (isLight) {
+      document.documentElement.setAttribute("data-color-mode", "dark");
+    } else {
+      document.documentElement.setAttribute("data-color-mode", "light");
+    }
+
     setIsLight((prev) => !prev);
   };
 
   return (
-    <div className="sticky top-0 px-4 sm:px-8 lg:px-12 py-3 flex justify-between text-white bg-[#009485] z-50">
+    <div className="sticky border-b-[1px] border-[#3C3C3C] top-0 px-4 sm:px-8 lg:px-12 py-3 flex justify-between text-white bg-[#1F1F1F] z-50">
       <div className="flex items-center space-x-4">
         <h1 className="text-xl font-bold tracking-wide">Better Algo.</h1>
       </div>
