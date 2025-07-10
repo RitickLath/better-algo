31411338699e7c295b3523df0a3259947f97f88c
RitickLath <ritick943lath@gmail.com>
2025-07-11 00:12:10 +0530

    Build Error Resolved

--- src/app/(topics)/arrays/[slug]/page.tsx
+++ src/app/(topics)/arrays/[slug]/page.tsx
@@ -1,52 +1,7 @@
 import MarkdownWrapper from "../../../component/MarkdownWrapper";
-
 import fs from "node:fs";
 import path from "node:path";
-
-export const arrayMarkdownMapping: Record<string, string> = {
-  // Array Theory
-  "array-theory": "Array-Theory/INTRODUCTION.md",
-  "array-creation": "Array-Theory/ARRAY CREATION.md",
-  "array-input-output": "Array-Theory/ARRAYS INPUT, OUTPUT AND UPDATES.md",
-  "array-function-argument": "Array-Theory/ARRAY AS FUNCTIONAL ARGUMENT.md",
-  "why-array-index-starts-at-0":
-    "Array-Theory/WHY ARRAY INDEX STARTS FROM 0 AND NOT 1.md",
-  "what-32bit-62bit-mean": "Array-Theory/WHAT 32-BIT AND 62-BIT RAM MEAN.md",
-
-  // Array Beginner
-  "array-linear-search": "Array-Beginner/LINEAR SEARCH.md",
-  "largest-in-array": "Array-Beginner/LARGEST IN AN ARRAY.md",
-  "second-largest-in-array":
-    "Array-Beginner/SECOND LARGEST IN AN ARRAY (SINGLE LOOP).md",
-  "reverse-array": "Array-Beginner/REVERSE AN ARRAY.md",
-  "check-sorted": "Array-Beginner/CHECK SORTED OR NOT.md",
-  "min-increment-k-ops":
-    "Array-Beginner/REMOVE MINIMUM INCREMENT BY K OPERATIONS TO MAKE ALL EQUAL.md",
-  "remove-duplicates-sorted": "Array-Beginner/REMOVE DUPLICATES FROM SORTED.md",
-  "rotate-array-by-one": "Array-Beginner/ROTATE THE ARRAY BY ONE PLACE.md",
-  "rotate-array-by-k": "Array-Beginner/ROTATE THE ARRAY BY K PLACE.md",
-  "move-zeros-to-end":
-    "Array-Beginner/MOVE ALL ZERO AT THE END OF THE ARRAY.md",
-  "find-missing-number": "Array-Beginner/FIND MISSING NUMBER.md",
-  "max-consecutive-ones": "Array-Beginner/MAXIMUM CONSECUTIVE ONES.md",
-
-  // Array Intermediate
-  "union-two-sorted-arrays": "Array-Intermediate/UNION OF TWO SORTED ARRAY.md",
-  "intersection-two-sorted-arrays":
-    "Array-Intermediate/INTERSECTION OF TWO SORTED ARRAY.md",
-  "pairs-in-array": "Array-Intermediate/PAIRS IN ARRAY.md",
-  "print-all-subarrays": "Array-Intermediate/PRINT SUBARRAYS (IMP).md",
-  "max-subarray-sum-naive":
-    "Array-Intermediate/MAX SUBARRAY SUM - I (NATIVE).md",
-  "max-subarray-sum-prefix":
-    "Array-Intermediate/MAX SUBARRAY SUM - II (PREFIX SUM).md",
-  "max-subarray-sum-kadane":
-    "Array-Intermediate/MAX SUBARRAY SUM - III (KADANE’S).md",
-  "dutch-national-flag-sort":
-    "Array-Intermediate/SORT AN ARRAY OF 0’S, 1’S AND 2’S  DUTCH FLAG.md",
-  "moores-voting-majority-element":
-    "Array-Intermediate/MAJORITY ELEMENT  MOORE’S VOTING ALGORITHM.md",
-};
+import { arrayMarkdownMapping } from "../../../lib/arrayMarkdownMapping";
 
 export default async function Arrays({ params }: { params: { slug: string } }) {
   const { slug } = await params;
